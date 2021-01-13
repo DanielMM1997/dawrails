204a76090cce285838d37c997f311dded4ea5571
Damon Garcia Garcia <damon99@hotmail.es>
2021-01-13 18:19:21 +0000

    Login de usuario

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,15 +5,17 @@ class SessionController < ActionController::Base
       render layout: 'application'
   end
   
-  def login
+  def new
       @categories = Category.all
       render layout: 'application'
   end
 
   def create
     @user = User.find_by(nickname: params[:nickname])
-    if @user && @user.authenticate(params[:nickname])
-       sessions[:user_id] = @user.id
+    @pass = params[:password]
+    @pass = Digest::SHA256.new << @pass
+    if @pass == @user.password
+       session[:user_id] = @user.id
        redirect_to welcome_path
     else
        redirect_to login_path
