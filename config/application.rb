afe1a8d41d7abfa1da7588cd4c0648a0eb0aacf6
Damon Garcia Garcia <damon99@hotmail.es>
2021-01-17 18:55:08 +0000

    Instalando Heroku v3

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,5 @@ module Dawrails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = false
-    config.assets.initialize_on_precompile = false
   end
 end
